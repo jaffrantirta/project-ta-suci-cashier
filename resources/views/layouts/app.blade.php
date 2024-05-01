e5b32476b17ff165a255afcd6158dfb9ce65b6a0
jaffrantirta <jaffrantirta@icloud.com>
2024-05-01 09:57:01 +0800

    fresh design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,9 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
@@ -77,31 +80,38 @@
         </nav>
 
         <div class="container-fluid">
-            <div class="row">
+            <div class="row ">
                 <!-- Sidebar -->
-                <div class="col-md-3 sidebar mt-4" style="position: fixed; height: 100%; overflow-y: auto; padding-top: 80px; padding-left: 15px;">
+                <div class="bg-dark col-md-3 sidebar mt-4" style="position: fixed; height: 100%; overflow-y: auto; padding-top: 80px; padding-left: 15px;">
                     <ul class="nav flex-column gap-1">
                         <li class="nav-item">
-                            <a class="nav-link text-dark border rounded {{ Route::currentRouteName() === 'home' ? 'bg-primary' : '' }}" href="{{ route('home') }}">Dashboard</a>
+                            <li class="nav-item">
+                                <a class="nav-link text-light rounded {{ Route::currentRouteName() === 'home' ? 'bg-primary' : '' }}" href="{{ route('home') }}">
+                                    <i class="fas fa-home me-2"></i>
+                                    Dashboard
+                                </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-dark border rounded {{ Route::currentRouteName() === 'transaction.index' ? 'bg-primary' : '' }}" href="{{ route('transaction.index', ['sort' => '-created_at']) }}">Transaksi</a>
+                            <a class="nav-link text-light rounded {{ Route::currentRouteName() === 'transaction.index' ? 'bg-primary' : '' }}" href="{{ route('transaction.index', ['sort' => '-created_at']) }}"><i class="fas fa-list me-2"></i>Transaksi</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-dark border rounded {{ Route::currentRouteName() === 'transaction.create' ? 'bg-primary' : '' }}" href="{{ route('transaction.create') }}">Tambah Transaksi Baru</a>
+                            <a class="nav-link text-light rounded {{ Route::currentRouteName() === 'transaction.create' ? 'bg-primary' : '' }}" href="{{ route('transaction.create') }}"><i class="fas fa-plus me-2"></i>Tambah Transaksi Baru</a>
                         </li>
-                        <li class="nav-item bg-light border rounded" id="stok-menu">
-                            <a class="nav-link text-dark border rounded {{ Route::currentRouteName() === 'stock.index' ? 'bg-primary' : '' }}" href="#" onclick="toggleSubMenu('stok-submenu')">Stok</a>
+                        <li class="nav-item bg-dark rounded" id="stok-menu">
+                            <a class="nav-link text-light d-flex justify-content-between rounded {{ Route::currentRouteName() === 'stock.index' ? 'bg-primary' : '' }}" href="#" onclick="toggleSubMenu('stok-submenu')">
+                                <div><i class="fas fa-cubes-stacked me-2"></i>Stok</div>
+                                <div><i class="fas fa-chevron-down me-2"></i></div>
+                            </a>
                             <ul class="nav flex-column gap-1 pl-3" id="stok-submenu" style="display: none;">
                                 <li class="nav-item">
-                                    <a class="nav-link text-dark border rounded {{ request()->input('filter.type') === '>' ? 'bg-primary' : '' }}"
+                                    <a class="nav-link text-light rounded {{ request()->input('filter.type') === '>' ? 'bg-secondary' : '' }}"
                                         href="{{ route('stock.index', ['filter[type]' => '>', 'sort' => '-created_at']) }}">
                                         Stok Masuk
                                      </a>
 
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link text-dark border rounded {{ request()->input('filter.type') === '<' ? 'bg-primary' : '' }}"
+                                    <a class="nav-link text-light rounded {{ request()->input('filter.type') === '<' ? 'bg-secondary' : '' }}"
                                         href="{{ route('stock.index', ['filter[type]' => '<', 'sort' => '-created_at']) }}">
                                         Stok Keluar
                                      </a>
@@ -109,10 +119,10 @@
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-dark border rounded {{ Route::currentRouteName() === 'item.index' ? 'bg-primary' : '' }}" href="{{ route('item.index') }}">Barang</a>
+                            <a class="nav-link text-light rounded {{ Route::currentRouteName() === 'item.index' ? 'bg-primary' : '' }}" href="{{ route('item.index') }}"><i class="fas fa-boxes-stacked me-2"></i>Barang</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-dark border rounded {{ Route::currentRouteName() === 'report.index' ? 'bg-primary' : '' }}" href="{{ route('report.index') }}">Laporan</a>
+                            <a class="nav-link text-light rounded {{ Route::currentRouteName() === 'report.index' ? 'bg-primary' : '' }}" href="{{ route('report.index') }}"><i class="fas fa-file me-2"></i>Laporan</a>
                         </li>
                     </ul>
                 </div>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -16,6 +16,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
